efabb02abaed2e3cc0f43bf721f99dea800cf089
David Bankson <62538124+QuothTheRaven42@users.noreply.github.com>
2026-03-24 22:54:37 -0700

    Update RPG Character Creator.py
    added:
    inventory methods
    @staticmethod dice roller
    dictionary for inventory
    healing method
    saving character sheet to txt file

--- RPG Character Creator.py
+++ RPG Character Creator.py
@@ -17,6 +17,7 @@ class Character:
         self.level = 1
         self.exp = 0
 
+        # Race bonuses
         if self.race == "Human":
             self.strength += 1
             self.constitution += 1
@@ -41,30 +42,39 @@ class Character:
             self.constitution += 1
             self.charisma += 2
 
-        self.inventory = ['50 ft rope', 'small health potion', 'small health potion', 'small health potion', 'small health potion', 'torch', 'water' , 'water' , 'water' , 'rations']
+        # Starting inventory
+        self.inventory = {'50 ft rope':1, 'small health potion':4, 'torch': 2, 'water':3, 'rations':1}
 
     def __str__(self):
         return self.char_sheet
 
     @property
-    def char_sheet(self):
+    def char_sheet(self) -> str:
         char_sheet = f"""\n{self.name} - {self.race} {self.__class__.__name__.lower()} - level {self.level}
 ---------------------------------
-    Health: {self.life_total}/{self.health}
-    Strength: {self.strength}
-    Dexterity: {self.dexterity}
-    Constitution: {self.constitution}
-    Intelligence: {self.intelligence}
-    Wisdom: {self.wisdom}
-    Charisma: {self.charisma}\n"""
+Health: {self.life_total}/{self.health}
+Strength: {self.strength}
+Dexterity: {self.dexterity}
+Constitution: {self.constitution}
+Intelligence: {self.intelligence}
+Wisdom: {self.wisdom}
+Charisma: {self.charisma}\n"""
         return char_sheet
 
-    def display_sheet(self):
+    def display_sheet(self) -> None:
         print(f'\nYour character sheet for {self.name} the {self.__class__.__name__.lower()}:')
         print(f'{self.char_sheet}')
 
+    def export_char_sheet(self) -> None:
+        with open(f"{self.name}_the_{self.race}_{self.__class__.__name__}_lvl{self.level}.txt", "w") as file:
+            file.write(self.char_sheet)
+            file.write('\n')
+            file.write('Inventory:\n')
+            lines = "\n".join(f"{value} {key}" for key, value in self.inventory.items())
+            file.write(f'{lines}')
+        print(f'Character sheet for {self.name} the {self.__class__.__name__.lower()} has been saved as {self.name}_the_{self.race}_{self.__class__.__name__}_lvl{self.level}.txt.')
 
-    def gain_exp(self, multiplier=1):
+    def gain_exp(self, multiplier=1) -> str | None:
         if self.passed_out == True:
             return f'{self.name} the {self.__class__.__name__.lower()} is passed out and cannot gain experience.'
         mult_value = 10 * multiplier
@@ -84,28 +94,28 @@ class Character:
             self.charisma += round(1 * (self.charisma * 0.08))
             self.life_total = self.health
 
-    def take_dmg(self, dmg):
+    def take_dmg(self, dmg) -> None:
         if self.life_total > 0:
             self.life_total -= dmg
         if self.passed_out == True:
-            return f'{self.name} the {self.__class__.__name__.lower()} is passed out and cannot take damage.'
+            print(f'{self.name} the {self.__class__.__name__.lower()} is passed out and cannot take damage.')
         elif self.life_total <= 0:
             self.life_total = 0
             self.passed_out = True
-            print(f'{self.name} the {self.__class__.__name__.lower()} is passed out!\n')
+            print(f'{self.name} the {self.__class__.__name__.lower()} took {dmg} damage and has passed out!\n')
             print(f'Life total: {self.life_total}/{self.health}')
         else:
             print(f'{self.name} the {self.__class__.__name__.lower()} has taken {dmg} points of damage!')
             print(f'Remaining life for {self.name}: {self.life_total}/{self.health}\n')
 
-    def rest(self):
+    def rest(self) -> None:
         print('\nResting up......')
         self.life_total = self.health
         self.passed_out = False
         print(f'{self.name} the {self.__class__.__name__.lower()} is rested up and ready to go!')
         print(f'Life total: {self.life_total}/{self.health}\n')
 
-    def cause_dmg(self, target):
+    def cause_dmg(self, target) -> int:
         if self.passed_out == True:
             print(f'{self.name} the {self.__class__.__name__.lower()} is passed out and cannot attack.')
             return 0
@@ -113,35 +123,55 @@ class Character:
             print(f'{self.name} has already won! {target.name} is passed out and cannot be attacked.')
             return 0
         else:
-            dmg = randint(1,5)
+            dmg = Character.roll_dice(6)
             print(f'{self.name} the {self.__class__.__name__.lower()} attacks for {dmg} hp!')
             target.take_dmg(dmg)
             self.gain_exp()
             return dmg
 
-    def use_item(self, item):
-        if item in self.inventory:
-            self.inventory.remove(item)
-            print(f'{self.name} the {self.__class__.__name__.lower()} used {item} from their inventory!\n')
+    def use_item(self, item) -> None:
+        if item not in self.inventory:
+            print(f"{item} is not {self.name}'s inventory.\n")
+        elif self.inventory[item] == 1:
+            del self.inventory[item]
+            print(f'{self.name} the {self.__class__.__name__.lower()} used {item} from their inventory.')
         else:
-            print(f'{item} is not in the inventory.\n')
+            self.inventory[item] -= 1
+            print(f'{self.name} the {self.__class__.__name__.lower()} used {item} from their inventory.')
+            print(f'There are {self.inventory[item]} {item} left in the inventory.')
 
-    def view_inventory(self):
-        items = ", ".join(self.inventory) if self.inventory else f"No items in {self.name}'s inventory."
-        return f"{self.name}'s inventory:\n{items}\n"
+        # healing items
+        if item == 'small health potion':
+            num = Character.roll_dice(6)
+            self.life_total = min(self.life_total + num, self.health)
+            print(f'small health potion used - +{num} health gained!')
+            print(f'Life total: {self.life_total}\n')
+            self.passed_out = False
+        elif item == 'large health potion':
+            num = Character.roll_dice(20) + Character.roll_dice(6)
+            self.life_total = min(self.life_total + num, self.health)
+            print(f'large health potion used - +{num} health gained!')
+            print(f'Life total: {self.life_total}\n')
+            self.passed_out = False
 
+    def add_item(self, item) -> None:
+        if item in self.inventory:
+            self.inventory[item] += 1
+        else:
+            self.inventory[item] = 1
+        print(f'{item} added to the inventory.')
+
+    def view_inventory(self) -> None:
+        if self.inventory:
+            lines = "\n".join(f"{value} {key}" for key, value in self.inventory.items())
+            print(f"{self.name}'s inventory:\n{lines}\n")
+        else:
+            print(f"No items in {self.name}'s inventory.\n")
 
     @staticmethod
-    def roll_d4():
-        return randint(1, 4)
+    def roll_dice(d_num) -> int:
+        return randint(1, d_num)
 
-    @staticmethod
-    def roll_d6():
-        return randint(1, 6)
-
-    @staticmethod
-    def roll_d20():
-        return randint(1, 20)
 
 class Barbarian(Character):
     def __init__(self):
@@ -162,6 +192,7 @@ class Cleric(Character):
         self.life_total = self.health
         self.display_sheet()
 
+
 class Wizard(Character):
     def __init__(self):
         super().__init__()
@@ -171,14 +202,16 @@ class Wizard(Character):
         self.life_total = self.health
         self.display_sheet()
 
+
 class Sorcerer(Character):
     def __init__(self):
         super().__init__()
         self.wisdom += 3
         self.health += 3
-        self.wisdom -= 3
+        self.dexterity -= 3
         self.life_total = self.health
         self.display_sheet()
+
 
 class Fighter(Character):
     def __init__(self):
@@ -189,6 +222,7 @@ class Fighter(Character):
         self.life_total = self.health
         self.display_sheet()
 
+
 class Rogue(Character):
     def __init__(self):
         super().__init__()
@@ -198,42 +232,14 @@ class Rogue(Character):
         self.life_total = self.health
         self.display_sheet()
 
-sorcerer = Sorcerer()
-
-sorcerer.gain_exp()
-sorcerer.gain_exp()
-sorcerer.gain_exp(multiplier=Character.roll_d4())
-sorcerer.gain_exp(multiplier=Character.roll_d6())
-sorcerer.gain_exp(multiplier=Character.roll_d20())
-
-sorcerer.take_dmg(10)
-sorcerer.take_dmg(10)
-sorcerer.take_dmg(10)
-sorcerer.take_dmg(10)
-
-print(sorcerer.char_sheet)
-print(sorcerer.view_inventory())
-
-sorcerer.rest()
-
-sorcerer.use_item('50 ft rope')
-sorcerer.use_item('water')
-sorcerer.use_item('potato')
-
-print(sorcerer.view_inventory())
-
 
 """
 Next steps:
 -------------
-* Add inventory methods
-* @staticmethod for dice roller
-
+Spells or abilities
+Monster class
 dictionary mapping the races to bonus stats instead of if/elif block
 @property setter for life_total, auto sets passed_out when at 0, cleaning up logic from take_dmg
 Class attributes for things like base_health or hit_die
-Spells or abilities
-Monster class
-Save/load character sheets with File I/O
 simple while loop for combat
 """
